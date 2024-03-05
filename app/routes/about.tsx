ebd8ab550039f2b4038392a0e8b50afe695dc8e9
Nicky Klaasse <n.klaasse@cvmaker.nl>
2024-03-05 18:39:19 +0100

    Add about page

---
+++ app/routes/about.tsx
@@ -0,0 +1,36 @@
+import { json } from "@remix-run/cloudflare";
+import type {
+  ActionFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/cloudflare";
+import { Button } from "~/components/Button/Button";
+import { Badge } from "~/components/Badge";
+import { IconSearch } from "~/icons/IconSearch";
+
+export const loader = async function loader() {
+  return json({});
+} satisfies LoaderFunction;
+
+export const action = async function action() {
+  return json({});
+} satisfies ActionFunction;
+
+export const meta = function meta() {
+  return [];
+} satisfies MetaFunction<typeof loader>;
+
+export default function Index() {
+  return (
+    <fieldset>
+      <legend>About</legend>
+      <Badge variant="info">Default</Badge>
+      <Button variant="primary">
+        <Button.Label>Primary</Button.Label>
+        <Button.Icon>
+          <IconSearch />
+        </Button.Icon>
+      </Button>
+    </fieldset>
+  );
+}
